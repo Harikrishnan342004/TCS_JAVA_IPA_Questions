7f4d623987dd78c6b1c7607020044240bcc344fb
Hari <harikrishnan53828@gmail.com>
2025-01-04 00:01:07 +0530

    jk

---
+++ MilitanMain.java
@@ -0,0 +1,21 @@
+  class Militant
+{
+    int militantId;
+    String name;
+    String category;
+    double experience;
+
+    public Militant(int militantId , String name , String category, double experience)
+    {
+       this.militantId = militantId;
+       this.name = name;
+       this.category = category;
+       this.experience = experience;
+    }
+}
+
+public class MilitanMain {
+    
+}
+
+
